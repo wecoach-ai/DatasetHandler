004a7f7156a8ae150426e4fdefc15b63fc23fa27
onlinejudge95 <onlinejudge95@gmail.com>
2024-07-17 00:47:33 +0530

    Adds skeleton for extract command

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,12 @@
 import click
 
-from src import download as dwnld
+from src.download import (
+    download_multiprocess,
+    generate_download_meta_data,
+    setup_dataset_directory,
+    unarchive_multiprocess,
+)
+from src.extract import extract_multiprocess, get_file_paths
 
 
 @click.group()
@@ -12,16 +18,18 @@ def cli():
 @click.argument("download_url", required=True)
 @click.argument("path", required=True)
 def download(download_url, path):
-    dwnld.setup_dataset_directory(path)
-    meta_data = dwnld.generate_download_meta_data(path, download_url)
+    setup_dataset_directory(path)
+    meta_data = generate_download_meta_data(path, download_url)
 
-    dwnld.download_multiprocess(meta_data)
-    dwnld.unarchive_multiprocess(meta_data)
+    download_multiprocess(meta_data)
+    unarchive_multiprocess(meta_data)
 
 
 @cli.command()
-def extract():
-    pass
+@click.argument("path", required=True)
+def extract(path):
+    file_paths = get_file_paths(path)
+    extract_multiprocess(file_paths)
 
 
 if __name__ == "__main__":
