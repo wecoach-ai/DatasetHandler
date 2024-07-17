a96c0ff77a57424e118cab841612abd09f6c13a4
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-07-17 12:19:18 +0530

    Add command for image extraction (#4)
    * Adds dependency for video manipulation
    
    * Adds skeleton for extract command
    
    * Adds function to extract frames using multiprocessing
    
    * Creates individual directories for extracting images
    
    * Fixed extraction path.
    
    * Options for image extraction.
    
    ---------
    
    Co-authored-by: Shreshtth <shreshtthpersonal@gmail.com>

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,12 @@
 import click
 
-from src import download as dwnld
+from src.download import (
+    download_multiprocess,
+    generate_download_meta_data,
+    setup_dataset_directory,
+    unarchive_multiprocess,
+)
+from src.extract import extract_multiprocess, generate_extract_meta_data
 
 
 @click.group()
@@ -12,16 +18,24 @@ def cli():
 @click.argument("download_url", required=True)
 @click.argument("path", required=True)
 def download(download_url, path):
-    dwnld.setup_dataset_directory(path)
-    meta_data = dwnld.generate_download_meta_data(path, download_url)
+    setup_dataset_directory(path)
+    meta_data = generate_download_meta_data(path, download_url)
 
-    dwnld.download_multiprocess(meta_data)
-    dwnld.unarchive_multiprocess(meta_data)
+    # download_multiprocess(meta_data)
+    unarchive_multiprocess(meta_data)
 
 
 @cli.command()
-def extract():
-    pass
+@click.argument("path", required=True)
+@click.option(
+    "--scope",
+    type=click.Choice(["all", "selected", "smooth"], case_sensitive=False),
+    default="all",
+    help="Select the type of image extraction (default=all)",
+)
+def extract(path, scope):
+    meta_data = generate_extract_meta_data(path)
+    extract_multiprocess(meta_data, scope)
 
 
 if __name__ == "__main__":
---
+++ src/extract.py
@@ -0,0 +1,53 @@
+import concurrent.futures
+import pathlib
+import typing
+
+import cv2
+
+
+def generate_extract_meta_data(path: str) -> typing.List[pathlib.Path]:
+    dataset_directory = pathlib.Path(path)
+    train_video_directory = dataset_directory / "train" / "videos"
+    test_video_directory = dataset_directory / "test" / "videos"
+
+    result = [video_file for video_file in train_video_directory.iterdir()] + [
+        video_file for video_file in test_video_directory.iterdir()
+    ]
+
+    for files in result:
+        image_directory = files.parent.parent / "images" / files.with_suffix("").name
+        image_directory.mkdir()
+
+    return result
+
+
+def extract_multiprocess(file_lists: typing.List[pathlib.Path], scope: str):
+    match scope:
+        case "all":
+            extract_fn = _extract_all_images
+        case "selected":
+            extract_fn = None
+        case "smooth":
+            extract_fn = None
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(extract_fn, file_lists)
+
+
+def _extract_all_images(video_file_path: pathlib.Path):
+    image_directory = (
+        video_file_path.parent.parent / "images" / video_file_path.with_suffix("").name
+    )
+
+    capture = cv2.VideoCapture(str(video_file_path))
+
+    counter = -1
+    while True:
+        flag, frame = capture.read()
+        if not flag:
+            break
+        counter += 1
+        image_path = image_directory / f"img_{counter:06d}.jpg"
+        cv2.imwrite(image_path, frame)
+
+    capture.release()
