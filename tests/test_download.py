293000277b7e13feffc6ab4439d901327ef27035
onlinejudge95 <onlinejudge95@gmail.com>
2024-07-19 12:31:46 +0530

    Fixes failing CI

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,2 +1,14 @@
-def test_true():
+def test_setup_dataset_directory() -> None:
+    assert True
+
+
+def test_generate_download_meta_data() -> None:
+    assert True
+
+
+def test__unarchive_files() -> None:
+    assert True
+
+
+def test_clean_archive() -> None:
     assert True
